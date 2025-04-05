c164d68854f78b422f73a8d8fbc2839c5d8ecd8b
LucioP7 <pianettilucio7@gmail.com>
2025-04-05 14:56:08 -0300

    Al dia

--- BackendTest/ClientesControllerTest.cs
+++ BackendTest/ClientesControllerTest.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Backend.Controllers;
+using Backend.DataContext;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +13,42 @@ namespace Backend.Tests
 {
     public class ClientesControllerTest
     {
-        [Fact]
-        public void GetClientes()
+        SqliteConnection connection = new SqliteConnection("DataSource=:memory:");
+        DbContextOptions<KioscoContext> options;
+
+        public ClientesControllerTest()
         {
+            connection.Open();
+            options = new DbContextOptionsBuilder<KioscoContext>()
+            .UseSqlite(connection)
+            .Options;
+        }
+
+        [Fact]
+        public async void GetClientes()
+        {
+            // Crear la base de datos y aplicar las migraciones
+            using (var context = new KioscoContext(options))
+            {
+                context.Database.EnsureCreated();
+
+                var controllerClientes = new ClientesController(context);
+                var result = await controllerClientes.GetClientes();
+                //convierto el result en una lista
+                var lista = result.Value.ToList();
+                //verifico que la lista no sea nula
+                Assert.NotNull(lista);
+                //verifico que la lista no este vacia
+                Assert.NotEmpty(lista);
+                //verifico que la lista tenga al menos un elemento
+                Assert.True(lista.Count > 0);
+                //verifico que la lista sea igual a 4
+                Assert.Equal(5, lista.Count);
+            }
+            // Cerrar la conexión al finalizar
+            connection.Close();
 
         }
     }
 }
+
